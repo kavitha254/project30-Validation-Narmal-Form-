b269f8f7127c108cb46687759c60c46053d7d849
kavitha254 <kavithakavithayadas@gmail.com>
2023-04-24 21:07:55 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+def check_for_a(value):
+    if value[0]=='a':
+         raise forms.ValidationError('name start with a')
+
+
+def check_for_len(value):
+    if len(value)<6:
+        raise forms.ValidationError('len is less')
+
+class StudentForm(forms.Form):
+    name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
+    age=forms.IntegerField()
