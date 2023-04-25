7b9188c1019fa389fb511e97415ac797e96b8040
kavitha254 <kavithakavithayadas@gmail.com>
2023-04-25 19:36:10 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,17 @@ def check_for_len(value):
 class StudentForm(forms.Form):
     name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
     age=forms.IntegerField()
+    email=forms.EmailField()
+    re_enter_email=forms.EmailField()
+    botcatchar=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('not matched')
+
+    def clean_botcatchar(self):
+        bot=self.cleaned_data['botcatchar']
+        if len(bot)>6:
+            raise ValidationError('bot')
